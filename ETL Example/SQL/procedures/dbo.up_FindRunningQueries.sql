4fd67f9c7f889105902a80202b05b200204500a4
Douglass Richlin <djrichlin@gmail.com>
2020-08-12 19:02:49 -0500

    ETL Example\SQL\procedures\dbo.up_FindRunningQueries.sql - Cleaned up the formatting, and added the ability to select more than one SPID for each run of the procedure and added the option to allow the user to ignore the calling session in the results

--- ETL Example/SQL/procedures/dbo.up_FindRunningQueries.sql
+++ ETL Example/SQL/procedures/dbo.up_FindRunningQueries.sql
@@ -1,72 +1,56 @@
 USE [ETL]
 GO
-
-/****** Object:  StoredProcedure [dbo].[up_FindRunningQueries]    Script Date: 12-Aug-20 01:08:51 AM ******/
+/****** Object:  StoredProcedure [dbo].[up_FindRunningQueries]    Script Date: 12-Aug-20 06:59:49 PM ******/
 SET ANSI_NULLS ON
 GO
-
 SET QUOTED_IDENTIFIER ON
 GO
-
-create procedure [dbo].[up_FindRunningQueries]
-	@SPID varchar(10)
-as
-begin
-	declare @SqlHandle varbinary(64) = (select sql_handle from etl.sys.sysprocesses where spid = @SPID)
-
-	;with cteSysProcesses as
+--up_FindRunningQueries @SPID = null, @IncludeCallingSession = 0
+ALTER PROCEDURE [dbo].[up_FindRunningQueries]
+	@SPID VARCHAR(10)
+	,@IncludeCallingSession BIT
+AS
+BEGIN
+	;WITH cteRunningSPIDs AS
 	(
 		select
-			right(convert(varchar, dateadd(ms, datediff(ms, last_batch, getdate()), '1900-01-01'), 121), 12) as [Batch Duration]
-			,hostname as [Calling Machine]
-			,hostprocess as [Calling Process Id]
-			,cmd as [Command Type]
-			,[dbid] as [Database Id]
-			,blocked as [Is Process Blocked]
-			,nt_username as [Network Username]
-			,open_tran as [Open Transaction]
-			,[status] as [Query Status]
-			,[program_name] as [Running Program]
-			,@SqlHandle as [sql_handle]
-			,loginame as [Sql Login]
-		from sys.sysprocesses
-		where spid = @SPID
+			spid AS [Session Id]
+			,RIGHT(CONVERT(VARCHAR, DATEADD(MILLISECOND, DATEDIFF(MILLISECOND, last_batch, GETDATE()), '1900-01-01'), 121), 12) AS [Batch Duration]
+			,hostname AS [Calling Machine]
+			,hostprocess AS [Calling Process Id]
+			,cmd AS [Command Type]
+			,[dbid] AS [Database Id]
+			,blocked AS [Is Process Blocked]
+			,nt_username AS [Network Username]
+			,open_tran AS [Open Transaction]
+			,[status] AS [Query Status]
+			,[program_name] AS [Running Program]
+			,loginame AS [Sql Login]
+		FROM sys.sysprocesses
+		-- ignore system functions and non-running sessions
+		WHERE [status] NOT IN ('background','sleeping')
+			AND cmd NOT IN ('awaiting command''mirror handler','lazy writer','checkpoint sleep','ra manager')
+			-- if a SPID is passed, ensure it is in the results
+			AND spid LIKE (CASE WHEN @SPID <> NULL THEN @SPID ELSE '%' END)
+			-- if the calling SPID is not to be used, do not include it
+			AND spid NOT LIKE (CASE WHEN @IncludeCallingSession = 0 THEN CONVERT(VARCHAR, @@SPID) ELSE '' END)
 	)
-	,cteSqlText as
-	(
-		select
-			@SqlHandle as [sql_handle]
-			,[text] as [Query Text]
-		from sys.dm_exec_sql_text(@SqlHandle)
-	), cteDatabases as
-	(
-		select
-			name as [Database Name]
-			,database_id as [Database Id]
-		from sys.databases
-	)
-	select
-		cteSysProcesses.[Batch Duration]
-		,cteSysProcesses.[Calling Machine]
-		,cteSysProcesses.[Calling Process Id]
-		,cteSysProcesses.[Command Type]
-		,cteDatabases.[Database Name]
-		,cteSysProcesses.[Is Process Blocked]
-		,cteSysProcesses.[Network Username]
-		,cteSysProcesses.[Open Transaction]
-		,cteSysProcesses.[Query Status]
-		,cteSqlText.[Query Text]
-		,cteSysProcesses.[Running Program]
-		,cteSysProcesses.[Sql Login]
-	from cteSqlText
-	inner join cteSysProcesses
-		on cteSqlText.sql_handle = cteSysProcesses.sql_handle
-	inner join cteDatabases
-		on cteSysProcesses.[Database Id] = cteDatabases.[Database Id]
-	where cteSysProcesses.[Query Status] not in ('background','sleeping')
-		and cteSysProcesses.[Command Type] not in ('awaiting command''mirror handler','lazy writer','checkpoint sleep','ra manager')
-end
-
-GO
-
-
+	SELECT
+		spids.[Session Id]
+		,spids.[Batch Duration]
+		,spids.[Calling Machine]
+		,spids.[Calling Process Id]
+		,dbs.name AS [Database Name]
+		,spids.[Is Process Blocked]
+		,spids.[Network Username]
+		,spids.[Open Transaction]
+		,spids.[Query Status]
+		,(SELECT [text] FROM sys.dm_exec_sql_text(reqs.[sql_handle])) AS [Query Text]
+		,spids.[Running Program]
+		,spids.[Sql Login]
+	FROM sys.databases dbs
+	INNER JOIN cteRunningSPIDs spids
+		ON dbs.database_id = spids.[Database Id]
+	INNER JOIN sys.dm_exec_requests reqs
+		ON spids.[Session Id] = reqs.session_id
+END
